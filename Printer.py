438e650c9dd6497d8150b2f7cc24d7a40c16cc86
Łukasz Matysiak <december0123@gmail.com>
2016-10-25 18:57:59 +0200

    Refactor and add tests

---
+++ Printer.py
@@ -0,0 +1,14 @@
+# This is the heart of the program.
+# This class is responsible for printing the string provided by the caller.
+# It is a proxy for the print function.
+
+
+class Printer:
+    # __init__ is responsible for initializing the _printer member, so we can use it later.
+    def __init__(self):
+        pass
+
+    # __call__ is used to print the string provided by the caller
+    def __call__(self, string_to_print):
+        print(string_to_print)
+
--- main.py
+++ main.py
@@ -4,26 +4,8 @@
 # Originally written by kmachnicki
 # Disclaimer: Yes, it is a joke :)
 
+from PrinterFactory import PrinterFactory
 
-# This is the heart of the program.
-# This class is responsible for printing the string provided by the caller.
-# It is a proxy for the print function.
-class Printer:
-    # __init__ is responsible for initializing the _printer member, so we can use it later.
-    def __init__(self):
-        self._printer = print
-
-    # __call__ is used to print the string provided by the caller
-    def __call__(self, string_to_print):
-        self._printer(string_to_print)
-
-# Since Printer is a pretty complicated class, we need a factory so we can create it easier.
-class PrinterFactory:
-    # create_printer is a static method of the PrinterFactory class
-    # it creates a printer and returns it to the caller
-    @staticmethod
-    def create_printer():
-        return Printer()
 
 # main function
 def main():
